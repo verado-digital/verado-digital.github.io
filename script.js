4753f8c75368f75810d4f5b9adff1734a082fbf8
verado-digital <alexnova2781@gmail.com>
2026-08-21 11:12:20 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -125,3 +125,14 @@ function initPopup(){
     }
   });
 }
+
+// Tawk.to live chat
+var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+(function(){
+  var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+  s1.async = true;
+  s1.src = 'https://embed.tawk.to/6a881e43b56df5344af1c0fe/1k0hrce1g';
+  s1.charset = 'UTF-8';
+  s1.setAttribute('crossorigin', '*');
+  s0.parentNode.insertBefore(s1, s0);
+})();
